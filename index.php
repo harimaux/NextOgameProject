3e6405e74417e0e1b56cd1f2f69f47a4e425238b
harimaux <lukaszwitek20@googlemail.com>
2022-09-18 20:39:07 +0100

    init1.0

--- index.php
+++ index.php
@@ -15,29 +15,42 @@ if (!isset($_SESSION)) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vinyls Online - Login</title>
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Share+Tech&display=swap" rel="stylesheet">
+
     <link href="css/style.css" rel="stylesheet">
 </head>
 
 <body>
+
+    <div class="indexBackground">
+
+    </div>
 
     <?php
 
     if (!isset($_SESSION['username'])) {
         ?>
 
-    <form class="indexLoginForm" action="login_script.php" method="POST">
-        <input type="email" name="email" placeholder="Email">
-        <input type="password" name="password" placeholder="Password">
-        <input type="submit" name="login_user" value="Login">
-    </form>
+    <div class="indexFormBox">
 
-    <a href="register.php">Register</a>
+        <form action="login_script.php" method="POST">
+            <input type="email" name="email" placeholder="Email">
+            <input type="password" name="password" placeholder="Password">
+            <input type="submit" name="login_user" value="Login">
+        </form>
 
+        <p>Don't have an account yet? <span><a href="register.php">Register</a></span></p>
+    </div>
     <?php } else { ?>
-    <a href="logout.php">Logout</a>
+
+    <div class="indexFormBox">
+        <a href="logout.php">Logout</a>
+    </div>
+
+
     <?php
     }
 
